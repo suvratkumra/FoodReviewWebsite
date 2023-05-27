80e0da370098eee2a75759d878fd6be7c8d76b42
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-05-26 21:36:49 -0400

    chore: adding the boilerplate files, CLIENT, SERVER

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+
+// all the middlewares we need. 
+
+// for post requests
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("This is what you expected");
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, console.log(`You are connected to the server at port ${port}`));
