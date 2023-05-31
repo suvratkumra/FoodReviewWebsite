19402cd25c722da0896b9a3739831fea93964ac3
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-05-31 11:43:11 -0400

    chore: more refactoring

--- server/routes/Profile/Profile.js
+++ server/routes/Profile/Profile.js
@@ -1,13 +1,8 @@
 const { Router } = require('express');
-const User = require('../../model/UserModel');
-const customResponse = require('../../utils/responseTemplate');
-const customError = require('../../utils/errorTemplate');
 const authorization = require('../../utils/protected');
-const jwt = require('jsonwebtoken');
 const updateProfileCtrl = require('../../controller/profileCtrl');
 
 const profileRouter = Router();
-
 
 // UPDATE PROFILE
 // triggered from settings page, when user wants to update some of their data.
