97f379d642aea07ab971d60785c73cad57cfdea9
suvratkumra <suvratkumra2018@gmail.com>
2023-06-07 08:02:18 -0400

    fix: adding some timeout so that authorization page doesnt flicker

--- client/src/components/authorization/Authorization.js
+++ client/src/components/authorization/Authorization.js
@@ -6,25 +6,30 @@ const Authorization = ({ children }) => {
     // get the token from the state of the authProvider we have.
     const { state } = useContext(AuthContext);
     const [tokenAvailable, setTokenAvailable] = useState(false);
+
+    // stuff which needs to be taken care of whenever it is available or every second
     useEffect(() => {
         setTokenAvailable(state.token !== null);
+        setTimeout(() => {
+            setIsPageReady(true);
+        }, 250);   // for 0.25 second.
     }, [state])
 
-    // token boolean
-    if (tokenAvailable) {
-        return (< div > {children} </div >)
-    }
-
     return (
-        <>
-            <h1>ERROR, you need to log in first.</h1>
-            <Link to="/login"> Login </Link>
-
-            <hr />
-            <h1> New user? </h1>
-            <Link to="/signup"> register today! </Link>
-        </>
+        <div>
+            {tokenAvailable ?
+                (< div > {children} </div >) :
+                (<div>
+                    <h1>ERROR, you need to log in first.</h1>
+                    <Link to="/login"> Login </Link>
+                    <h1> New user? </h1>
+                    <Link to="/signup"> register today! </Link>
+                    <hr />
+                </div>)
+            }
+        </div>
     )
+
 }
 
 export default Authorization
