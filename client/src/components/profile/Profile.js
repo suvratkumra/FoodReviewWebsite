90c3213b8e6ca3a3755c16272a706ba8943e19c3
suvratkumra <suvratkumra2018@gmail.com>
2023-06-08 14:41:17 -0400

    feat: making the profile page to display information

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,14 +1,35 @@
-import React, {useContext, useState, useEffect} from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { ProfileContext } from '../../contexts/profileContext/ProfileContext';
 
 const Profile = () => {
-    const { state, getProfileDetailsAction} = useContext(ProfileContext);
+    const { state, getProfileDetailsAction } = useContext(ProfileContext);
+
+    const [profileDetails, setProfileDetails] = useState({});
+    const [laodingSymbol, setLoadingSymbol] = useState(true);
+
     useEffect(() => {
-        getProfileDetailsAction();
-    }, []);
-  return (
-    <div></div>
-  )
+        getProfileDetailsAction()
+            .then((response) => {
+                setProfileDetails(response);
+            })
+            .catch((err) => {
+                setProfileDetails(err);
+            });
+    }, [state, profileDetails]);
+
+    return (
+        <div>
+            {Object.values(profileDetails).map((value, index) => {
+                {return (
+                    <div>
+                        <p>{value}</p>
+                        <p>{index}</p>
+
+                    </div>
+                )}
+            })}
+        </div>
+    )
 }
 
 export default Profile
--- client/src/contexts/profileContext/ProfileContext.js
+++ client/src/contexts/profileContext/ProfileContext.js
@@ -5,30 +5,43 @@ import { AuthContext } from "../authContext/AuthContext";
 
 const INITIAL_STATE = {
     email: null,
-    username: null, 
+    username: null,
     profilePic: null,
     address: null,
-    phoneNumber: null, 
+    phoneNumber: null,
     lists: [null],
     verification: null,
-    feedback: null
+    feedback: null,
+    error: null
 }
 
 export const ProfileContext = createContext();
 
 const reducer = (state, action) => {
-    switch (action.type)
-    {
+    switch (action.type) {
         case "GET_PROFILE_SUCCESS": {
             return {
                 ...state,
-                email: action.payload
+                email: action.payload.email,
+                username: action.payload.username,
+                profilePic: action.payload.profilePic,
+                address: action.payload.address,
+                phoneNumber: action.payload.phoneNumber,
+                lists: action.payload.lists,
+                verification: action.payload.verification,
+                feedback: action.payload.feedback
+            };
+        }
+        case "GET_PROFILE_REJECT": {
+            return {
+                ...state,
+                error: action.payload.error
             }
         }
     }
 }
 
-const ProfileContextProvider = ({children}) => {
+const ProfileContextProvider = ({ children }) => {
 
     const details = useContext(AuthContext);
     const userState = details.state;
@@ -36,30 +49,34 @@ const ProfileContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
     const getProfileDetailsAction = async () => {
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": userState.token
-            },
-        };
-        try {
-            const response = await axios.get(`http://localhost:3000/v1/profile/${userState.profileID}`, config);
-
-            console.log(response.data.response[0].userProfile);
-            dispatch({
-                type: "GET_PROFILE_SUCCESS",
-                payload: response?.data?.response[0]?.userProfile
-            });
-            console.log("State ", userState);
-
-        } catch (error) {
-            // handle the error
-        }
+        return new Promise((resolve, reject) => {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": userState.token
+                },
+            };
+            axios.get(`http://localhost:3000/v1/profile/${userState.profileID}`, config)
+                .then((response) => {
+                    dispatch({
+                        type: "GET_PROFILE_SUCCESS",
+                        payload: response?.data?.response[0]?.userProfile
+                    });
+                    resolve(state);
+                })
+                .catch((err) => {
+                    dispatch({
+                        type: "GET_PROFILE_REJECT",
+                        payload: err
+                    })
+                    reject(state);
+                });
+        })
 
     }
 
     return (
-        <ProfileContext.Provider value = {{state, getProfileDetailsAction}}>
+        <ProfileContext.Provider value={{ state, getProfileDetailsAction }}>
             {children}
         </ProfileContext.Provider>
     )
