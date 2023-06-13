09c4d549c76b21a020589caa5c4531e3101f2c16
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-06-13 08:41:28 -0400

    fix: removing google api. Making own json data for Toronto based restaurants

--- server/routes/Restaurants/Restaurants.js
+++ server/routes/Restaurants/Restaurants.js
@@ -15,24 +15,7 @@ restRouter.get("/nearby/", async (req, res) => {
     // define the query parameters
     const { location, radius, pageToken } = req.query;
 
-    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, {
-        params: {
-            location: location,
-            radius: radius,
-            type: 'restaurant|cafe|bakery|bar|fast_food|meal_takeaway|meal_delivery|food',
-            key: process.env.GEOLOCATION_MAPS_API_KEY,
-            // pageToken: pageToken
-        }
-    })
-        .then((response) => {
-            const data = response.data;
-            customResponse(req, res, 200, "Authorized",
-                { dataReceived: data }
-            );
-        })
-        .catch((error) => {
-            customError(req, res, error?.code, error?.message);
-        })
+
 })
 
 module.exports = restRouter;
