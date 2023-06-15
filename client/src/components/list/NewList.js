7819297d3f9c1e7e4c4abbd638971f83ffb156c5
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-06-14 22:15:37 -0400

    chore: refactoring to avoid redundant code

--- client/src/components/list/NewList.js
+++ client/src/components/list/NewList.js
@@ -19,6 +19,35 @@ const NewList = () => {
         console.log("dish", dishImages)
     }
 
+    const getAllSpiceLevels = () => {
+        return (
+            <select>
+                <option value="nothing_selected">Nothing Selected</option>
+                <option value="bland">Bland</option>
+                <option value="mild">Mild</option>
+                <option value="medium">Medium</option>
+                <option value="hot">Hot</option>
+                <option value="spicy">Spicy</option>
+                <option value="fiery">Fiery</option>
+                <option value="zesty">Zesty</option>
+                <option value="tangy">Tangy</option>
+                <option value="peppery">Peppery</option>
+                <option value="piquant">Piquant</option>
+                <option value="flaming">Flaming</option>
+                <option value="scorching">Scorching</option>
+                <option value="inferno">Inferno</option>
+                <option value="blaze">Blaze</option>
+                <option value="nuclear">Nuclear</option>
+                <option value="volcanic">Volcanic</option>
+                <option value="scalding">Scalding</option>
+                <option value="incendiary">Incendiary</option>
+                <option value="mouth-numbing">Mouth-numbing</option>
+                <option value="explosive">Explosive</option>
+                <option value="insane">Insane</option>
+            </select>
+        )
+    }
+
     return (
         <div>
             This is the new list for restaurant: <span style={{ color: "red" }}> {queryParams.restaurantName} </span>
@@ -26,46 +55,25 @@ const NewList = () => {
                 <span>SORT BY</span>
                 <div>
                     Category:
-                    <select>
-                        <option value="nothing_selected">Nothing Selected</option>
-                        <option value="bland">Bland</option>
-                        <option value="mild">Mild</option>
-                        <option value="medium">Medium</option>
-                        <option value="hot">Hot</option>
-                        <option value="spicy">Spicy</option>
-                        <option value="fiery">Fiery</option>
-                        <option value="zesty">Zesty</option>
-                        <option value="tangy">Tangy</option>
-                        <option value="peppery">Peppery</option>
-                        <option value="piquant">Piquant</option>
-                        <option value="flaming">Flaming</option>
-                        <option value="scorching">Scorching</option>
-                        <option value="inferno">Inferno</option>
-                        <option value="blaze">Blaze</option>
-                        <option value="nuclear">Nuclear</option>
-                        <option value="volcanic">Volcanic</option>
-                        <option value="scalding">Scalding</option>
-                        <option value="incendiary">Incendiary</option>
-                        <option value="mouth-numbing">Mouth-numbing</option>
-                        <option value="explosive">Explosive</option>
-                        <option value="insane">Insane</option>
-                    </select>
+                    {getAllSpiceLevels()}
                 </div>
             </div>
             <hr />
             <div id='main_container'>
-                <div id="queue_card_container" >
+                <div id="queue_card_container" style={{ width: "50%" }}>
                     <div style={{ display: 'flex' }}>
-                        <div id="dish_image">
+                        <div id="dish_image" >
                             {dishImages.map((file) => (
-                                <img key={file.name} src={URL.createObjectURL(file)} alt={file.name} style={{ width: "100px", height: "100px" }} />
+                                <img key={file.name} src={URL.createObjectURL(file)} alt={file.name} style={{ width: "100px", height: "100px", padding: "2%" }} />
                             ))}
                         </div>
-                        <div style={{display:'flex', flexDirection: "column"}}>
-                            <label form='dishname'>Dish Name: </label>
-                            <input id='dishname' placeholder='Grilled Cheese'/>
-                            <label form='notes'>Notes: </label>
-                            <input id='notes' placeholder='notes' />
+                        <div style={{ display: 'flex', flexDirection: "column" }}>
+                            <label htmlFor='dishname'>Dish Name: </label>
+                            <input id='dishname' placeholder='Grilled Cheese' />
+                            <label htmlFor='notes'>Notes: </label>
+                            <textarea style={{ width: "300px", height: "150px", }} placeholder='Notes'></textarea>
+                            <label htmlFor='tags'>Tags: </label>
+                            {getAllSpiceLevels()}
                         </div>
 
                     </div>
