3f44348af52fc9d58c3615cff36fff567d05c164
suvratkumra <suvratkumra2018@gmail.com>
2023-06-19 12:04:35 -0400

    feat: disabling button for 15 seconds before sending another verification link

--- client/src/components/verify/Verification.js
+++ client/src/components/verify/Verification.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/authContext/AuthContext';
 import { ProfileContext } from '../../contexts/profileContext/ProfileContext'
@@ -11,10 +11,22 @@ const Verification = () => {
     const [invalidCodeMessage, setInvalidCodeMessage] = useState("");
     const [anotherCodeButton, setAnotherCodeButton] = useState(true);
     const [verificationEmailSent, setVerificationEmailSent] = useState("");
+    const [verificationCodeButtonEnabled, setVerficationCodeButtonEnabled] = useState(true);
     const payload = getProfileDetailsAction();
     const navigate = useNavigate();
 
-    const handleOnSubnitAction = async (e) => {
+    useEffect(() => {
+        setTimeout(() => {
+            setVerficationCodeButtonEnabled(false);
+            setVerificationEmailSent("")
+        }, 15000)
+
+        setTimeout(() => {
+        })
+    }, [verificationCodeButtonEnabled])
+
+
+    const handleOnSubmitAction = async (e) => {
         e.preventDefault();
 
         // TODO: CREATE A BANNER FOR UNVERIFIED USERS SO THAT IT REMINDS THEM TO VERIFY BEFORE THEY CAN ACCESS LISTS.
@@ -34,6 +46,7 @@ const Verification = () => {
         const bVerificationEmailSent = sendAnotherVerificationCodeAction();
         if (bVerificationEmailSent) {
             setVerificationEmailSent("A new verification link has been sent.")
+            setVerficationCodeButtonEnabled(true);
         }
         else {
             setVerificationEmailSent("There was an error sending a verification link, try again later.");
@@ -42,9 +55,9 @@ const Verification = () => {
 
     return (
         <>
-            <form onSubmit={handleOnSubnitAction}>
+            <form onSubmit={handleOnSubmitAction}>
                 {invalidCodeMessage}
-                <h4>Sent verification link to: <span style={{color: "blue"}}>{state.email}</span></h4>
+                <h4>Sent verification link to: <span style={{ color: "blue" }}>{state.email}</span></h4>
                 <label htmlFor='verificationCode'>Verification Code: </label>
                 <input type="text" name="verificationCode" id="verificationCode" value={verificationCode} onChange={(e) => {
                     setVerificationCode(e.target.value)
@@ -55,6 +68,7 @@ const Verification = () => {
             {anotherCodeButton && <div>
                 <button
                     onClick={handleSendingVerificationLinkAgain}
+                    disabled={verificationCodeButtonEnabled}
                 >
                     Click here to send Verification code again
                 </button>
