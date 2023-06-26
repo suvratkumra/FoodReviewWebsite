6c74773738b13f6bae26b345a2836ceecf9ab95d
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-06-26 14:43:05 -0400

    feat: changes to the home and navbar cs

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,61 @@
-
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-import Home from "./components/home/Home";
-import NewList from "./components/list/NewList";
-import Lists from "./components/list/Lists";
-import Navbar from "./components/navbar/Navbar";
-import Signup from "./components/signup/Signup";
-import Login from "./components/login/Login";
-import Authorization from "./components/authorization/Authorization";
-import Profile from "./components/profile/Profile";
-import About from "./components/about/About";
-import GlobalHandler from "./components/globalHandler/GlobalHandler";
-import Verification from "./components/verify/Verification";
-import './output.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/home/Home';
+import NewList from './components/list/NewList';
+import Lists from './components/list/Lists';
+import Navbar from './components/navbar/Navbar';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+import Authorization from './components/authorization/Authorization';
+import Profile from './components/profile/Profile';
+import About from './components/about/About';
+import GlobalHandler from './components/globalHandler/GlobalHandler';
+import Verification from './components/verify/Verification';
+import './output.css';
 
 function App() {
   return (
-    <div className="bg-gray-50">
+    <div className="bg-white min-h-screen">
       <BrowserRouter>
         <GlobalHandler />
         <Navbar />
         <Routes>
-          <Route path="/"
+          <Route
+            path="/"
             element={
               // <Authorization>
               <Home />
-              // </Authorization> 
-            } />
+              // </Authorization>
+            }
+          />
           <Route path="/newList" element={<NewList />} />
           <Route path="/lists" element={<Lists />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
 
-          <Route path="/about"
+          <Route
+            path="/about"
             element={
               <Authorization>
                 <About />
               </Authorization>
-            } />
-          <Route path="/profile"
+            }
+          />
+          <Route
+            path="/profile"
             element={
               <Authorization>
                 <Profile />
               </Authorization>
-            } />
-          <Route path="/new/list" element={
-            <Authorization>
-              <NewList />
-            </Authorization>
-          } />
+            }
+          />
+          <Route
+            path="/new/list"
+            element={
+              <Authorization>
+                <NewList />
+              </Authorization>
+            }
+          />
           <Route path="/verification-page" element={<Verification />} />
         </Routes>
       </BrowserRouter>
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,78 +1,76 @@
-
-import { Link } from 'react-router-dom'
-import { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
 import { RestContext } from '../../contexts/restaurantContext/RestContext';
-import loadingImage from '../../images/loading-sign.png'
+import loadingImage from '../../images/loading-sign.png';
 import { ListContext } from '../../contexts/listContext/ListContext';
 import { AuthContext } from '../../contexts/authContext/AuthContext';
 
 const Home = () => {
-    const { setRadiusAction, state, getLocationAction, searchRestaurantAction, getRestaurantsNearbyAction, } = useContext(RestContext);
+    const { setRadiusAction, state, getLocationAction, searchRestaurantAction, getRestaurantsNearbyAction } = useContext(RestContext);
     const [displayLoading, setDisplayLoading] = useState(true);
-    const [error, setError] = useState("")
+    const [error, setError] = useState('');
     const [radius, setRadius] = useState({ actualValue: 200, tempValue: 200 });
     const [runLocationFnAgain, setRunLocationFnAgain] = useState(true);
 
-    const [searchBarInput, setSearchBarInput] = useState("");
+    const [searchBarInput, setSearchBarInput] = useState('');
 
     // LIST CONTEXT
-    const listDetails = useContext(ListContext)
+    const listDetails = useContext(ListContext);
     const listState = listDetails.state;
     const createNewListAction = listDetails.createNewListAction;
 
     const { setUserTaskAction, USERTASKS } = useContext(AuthContext);
 
     useEffect(() => {
-        getLocationAction().then(() => {
-            if (state.latitude && state.longitude) {
-                // (state.latitude, state.longitude)
-                locationNearbyAction();
-            }
-            setRunLocationFnAgain(false);
-        })
+        getLocationAction()
+            .then(() => {
+                if (state.latitude && state.longitude) {
+                    // (state.latitude, state.longitude)
+                    locationNearbyAction();
+                }
+                setRunLocationFnAgain(false);
+            })
             .catch((err) => {
                 setError(err);
-            })
-
+            });
 
         const locationNearbyAction = () => {
-            getRestaurantsNearbyAction().then(() => {
-                setDisplayLoading(false);
-            })
+            getRestaurantsNearbyAction()
+                .then(() => {
+                    setDisplayLoading(false);
+                })
                 .catch((err) => {
                     setError(err);
                 });
-        }
-    }, [state.radius, runLocationFnAgain])
+        };
+    }, [state.radius, runLocationFnAgain]);
 
     useEffect(() => {
-        setRadiusAction(radius.actualValue)
-    }, [radius.actualValue])
+        setRadiusAction(radius.actualValue);
+    }, [radius.actualValue]);
 
     const handleRadiusOnChangeAction = (event) => {
         setRadius({
             ...radius,
-            tempValue: event.target.value
+            tempValue: event.target.value,
         });
-    }
+    };
 
     const handleUpdatedRadiusAction = (event) => {
         setRadius({
             ...radius,
-            actualValue: radius.tempValue
-        })
-    }
+            actualValue: radius.tempValue,
+        });
+    };
 
     // use the index to find the restaurant details from the object we already have in restaurant context.
     const handleNewListButtonOnClick = (restName, restIndex) => {
         createNewListAction(restName, restIndex);
         setUserTaskAction(USERTASKS.CREATE_NEW_LIST, restName, restIndex);
-    }
-
-
+    };
 
     const handleSearchSubmitting = () => {
-        if (searchBarInput !== "") {
+        if (searchBarInput !== '') {
             searchRestaurantAction(searchBarInput)
                 .then((response) => {
                     console.log(response);
@@ -80,45 +78,82 @@ const Home = () => {
                 .catch((error) => {
                     console.log(error);
                 });
-        }
-        else {
-            getRestaurantsNearbyAction().then(() => {
-                setDisplayLoading(false);
-            })
+        } else {
+            getRestaurantsNearbyAction()
+                .then(() => {
+                    setDisplayLoading(false);
+                })
                 .catch((err) => {
                     setError(err);
                 });
         }
-    }
+    };
 
     return (
-        <>
-            <h1>Welcome to the food review app </h1>
-            <label htmlFor='search-bar'>Search: </label>
-            <input type='search' id='search' onChange={(event) => setSearchBarInput(event.target.value)}></input>
-            <button onClick={handleSearchSubmitting}> Search </button>
-
-            <h1>Click on any restaurant to start creating your list</h1>
-
-            <p> Update the radius here for your search. </p>
-
-            <input type='range' id="range" min={200} max={2000} step={100} onChange={handleRadiusOnChangeAction} />
-
-            <input type='button' name="submitRadius" id="submitRadius" onClick={handleUpdatedRadiusAction} placeholder='Click to update radius' />
-            {displayLoading && <img src={loadingImage} alt="Loading" />}
-            {state.restaurants_nearby_names?.map((value, index) => {
-                return (
-                    <div style={{ border: "2px solid black", margin: "10px 5px", padding: "20px 5px", display: 'flex', justifyContent: 'space-around' }}>
-                        <span style={{ fontSize: "1.5rem" }} key={`${value} ${index}`}> {value} </span>
-                        <Link to={`/new/list?restaurant=${value}&index=${index}`}>
-                            <button onClick={() => handleNewListButtonOnClick(value, index)}>Create a new list</button>
-                        </Link>
-
+        <div className="bg-gray-50 min-h-screen">
+            <div className="container mx-auto px-4">
+                <h1 className="text-4xl text-center py-8">Welcome to the food review app</h1>
+                <div className="flex justify-center pb-4">
+                    <label htmlFor="search-bar" className="text-gray-700 mr-2 self-center">
+                        Search:
+                    </label>
+                    <div className="flex">
+                        <input
+                            type="search"
+                            id="search"
+                            onChange={(event) => setSearchBarInput(event.target.value)}
+                            className="border border-gray-300 rounded-l px-4 py-2 w-64 focus:outline-none"
+                        />
+                        <button
+                            onClick={handleSearchSubmitting}
+                            className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-r"
+                        >
+                            Search
+                        </button>
                     </div>
-                )
-            })}
-        </>
-    )
-}
+                </div>
+                <h1 className="text-2xl text-center py-4">Click on any restaurant to start creating your list</h1>
+                <p className="text-center pb-4">Update the radius here for your search.</p>
+                <div className="flex justify-center items-center pb-4">
+                    <input
+                        type="range"
+                        id="range"
+                        min={200}
+                        max={2000}
+                        step={100}
+                        onChange={handleRadiusOnChangeAction}
+                        className="w-64"
+                    />
+                    <button
+                        type="button"
+                        name="submitRadius"
+                        id="submitRadius"
+                        onClick={handleUpdatedRadiusAction}
+                        className="ml-2 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+                    >
+                        Click to update radius
+                    </button>
+                </div>
+                {displayLoading && <img src={loadingImage} alt="Loading" className="mx-auto" />}
+                <div className="flex flex-col items-center">
+                    {state.restaurants_nearby_names?.map((value, index) => (
+                        <div
+                            key={`${value} ${index}`}
+                            className="border border-gray-400 rounded-lg my-4 p-4 flex justify-between items-center w-full md:w-1/2"
+                        >
+                            <span className="text-xl">{value}</span>
+                            <Link
+                                to={`/new/list?restaurant=${value}&index=${index}`}
+                                className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+                            >
+                                Create a new list
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
